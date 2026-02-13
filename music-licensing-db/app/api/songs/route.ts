21006bc844d9220ef1eaefb5352a9b307ae5be1f
steveowenrva <steveowenrva@gmail.com>
2026-02-13 23:46:51 +0000

    edited route.ts & prisma.ts

---
+++ music-licensing-db/app/api/songs/route.ts
@@ -0,0 +1,15 @@
+import { prisma } from '@/lib/prisma'
+import { NextResponse } from 'next/server'
+
+
+export async function GET() {
+const songs = await prisma.song.findMany()
+return NextResponse.json(songs)
+}
+
+
+export async function POST(req: Request) {
+const data = await req.json()
+const song = await prisma.song.create({ data })
+return NextResponse.json(song)
+}

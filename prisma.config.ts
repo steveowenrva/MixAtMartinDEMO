4d195723ad6f78f298c42b109c472a0f738d7e9a
steveowenrva <steveowenrva@gmail.com>
2026-02-19 19:42:06 +0000

    idk man

--- prisma.config.ts
+++ prisma.config.ts
@@ -9,6 +9,6 @@ export default defineConfig({
     path: "prisma/migrations",
   },
   datasource: {
-    url: /workspaces/MixAtMartinDEMO/music-licensing-db/public/audio ["DATABASE_URL"],
+    url: process.env["DATABASE_URL"],
   },
 });

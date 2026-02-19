adbbae4d5d0f3f16368de4d7d5c4e769cc826f8c
steveowenrva <steveowenrva@gmail.com>
2026-02-19 16:17:24 +0000

    installed latest json packages

--- music-licensing-db/app/api/songs/route.ts
+++ music-licensing-db/app/api/songs/route.ts
@@ -1,15 +1,13 @@
 import { prisma } from '@/lib/prisma'
 import { NextResponse } from 'next/server'
 
-
 export async function GET() {
-const songs = await prisma.song.findMany()
-return NextResponse.json(songs)
+  const songs = await prisma.song.findMany()
+  return NextResponse.json(songs)
 }
 
-
 export async function POST(req: Request) {
-const data = await req.json()
-const song = await prisma.song.create({ data })
-return NextResponse.json(song)
+  const data = await req.json()
+  const song = await prisma.song.create({ data })
+  return NextResponse.json(song)
 }
